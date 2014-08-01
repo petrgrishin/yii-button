f9d123bc60a640ffb5534cbf7731d3f07e918614
Petr Grishin <petr.grishin@grishini.ru>
2014-08-01 23:34:54 +0400

    Реализация генерации html кода кнопки

--- src/ButtonWidget.php
+++ src/ButtonWidget.php
@@ -9,10 +9,12 @@ namespace PetrGrishin\Button;
 use PetrGrishin\Widget\BaseWidget;
 
 class ButtonWidget extends BaseWidget {
+    public $title;
     public $url;
 
     public function run() {
         $this->render('button', array(
+            'title' => $this->title,
             'url' => $this->url
         ));
     }
--- src/views/button.php
+++ src/views/button.php
@@ -3,6 +3,14 @@
  * @var \PetrGrishin\View\View $this
  * @author Petr Grishin <petr.grishin@grishini.ru>
  */
+use PetrGrishin\HtmlTag\HtmlTag;
+
+HtmlTag::create('button')
+    ->attr('id', $containerId = $this->getUniqueIdentifier('container'))
+    ->setContent($this->getParam('title'))
+    ->run();
+
 $this->setJsParams(array(
-    'url' => $this->getParam('url')
+    'containerId' => $containerId,
+    'url' => $this->getParam('url'),
 ));
